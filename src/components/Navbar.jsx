cc9f50ead198c6c116e9655060a86b3189438e59
Lucatonello <lucatonellow@gmail.com>
2024-10-18 16:01:47 -0300

    display info in Profile component and added styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,8 @@ function Navbar() {
             <img 
                 src={linkedInIcon} 
                 alt="LinkedIn Icon" 
-                className={styles.icon} 
+                className={styles.icon}
+                onClick={() => navigate('/')} 
                 style={{ height: '40.99px', width: '40.99px' }}
             />
         </div>
@@ -29,7 +30,7 @@ function Navbar() {
             {/* Navbar Icons */}
             <div className={styles.navItems}>
                 <div className={styles.navItem}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={styles.icon}>
+                    <svg onClick={() => navigate('/')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={styles.icon}>
                         <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
                     </svg>
                     <span>Home</span>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 import memberQueries from '../queries/memberQueries';
+import linkedInLogo from '../images/linkedin.png'
+import defaultBanner from '../images/default-banner.png';
+import Navbar from './Navbar';
+import styles from '../styles/Profile.module.css'; 
 
 function Profile() {
     const [memberData, setMemberData] = useState('');
@@ -25,9 +29,44 @@ function Profile() {
     }, [id, type])
 
     return (
-        <div>
-            <h1>{type == 'user' ? memberData.username : memberData.name}</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className={styles.profileContainer}>
+                <div className={styles.bannerContainer}>
+                    <img src={defaultBanner} className={styles.bannerImage} alt="" />
+                    <img 
+                        src={linkedInLogo} 
+                        className={styles.profilePicture} 
+                        alt="" 
+                    />
+                </div>
+                <h1 className={styles.username}>
+                    {type === 'user' ? memberData.username : memberData.name} Tonello
+                </h1>
+                <strong style={{ marginBottom: '10px', marginTop: '3px' }} className={styles.summary}>{memberData.summary} </strong>
+                <p className={styles.location}>{memberData.location}</p>
+                <a className={styles.website} href={memberData.website} target='__blank' style={{ color: '#0a66c2', textDecoration: 'none'}}>{memberData.website}</a>
+                <div style={{ display: 'flex' }}>
+                    <p className={styles.connections} style={{ color: '#0a66c2'}}>
+                        {memberData.connections_count} connections 
+                    </p>
+                    <p className={styles.connections} style={{ marginLeft: '10px'}}>
+                        {memberData.followers_count} followers
+                    </p>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+                    <path d="M200-200h50.46l409.46-409.46-50.46-50.46L200-250.46V-200Zm-60 60v-135.38l527.62-527.39q9.07-8.24 20.03-12.73 10.97-4.5 23-4.5t23.3 4.27q11.28 4.27 19.97 13.58l48.85 49.46q9.31 8.69 13.27 20 3.96 11.31 3.96 22.62 0 12.07-4.12 23.03-4.12 10.97-13.11 20.04L275.38-140H140Zm620.38-570.15-50.23-50.23 50.23 50.23Zm-126.13 75.9-24.79-25.67 50.46 50.46-25.67-24.79Z"/>
+                </svg>
+            </div>
+            <div className={styles.profileContainer}>
+                <h1 className={styles.titles} style={{ paddingTop: '10px'}}>About</h1>
+                <p className={styles.about}>{memberData.about}</p>
+            </div>
+            <div className={styles.profileContainer}>
+                <h1 className={styles.titles} style={{ paddingTop: '10px'}}>Activity</h1>
+                <p className={styles.about}>{memberData.about}</p>
+            </div>
+        </>
     );
 } 
 
